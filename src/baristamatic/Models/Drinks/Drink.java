2055d8b8943721ac146648cb4589d36653c91952
Carl Gramata <carlgramata@Carls-MacBook-Pro.local>
2019-02-12 23:34:15 -0600

    Adds comments to the Drink class

--- src/baristamatic/Models/Drinks/Drink.java
+++ src/baristamatic/Models/Drinks/Drink.java
@@ -11,27 +11,55 @@ import java.util.Map;
 import java.util.HashMap;
 
 /**
+ * The parent class for the coffee beverage classes 
  *
  * @author carlgramata
+ * @since 11Feb2019
  */
 public abstract class Drink implements Comparable<Drink>{
+    /**
+     * drinkName   represents the subclass drink name
+     * drinkPrice  represents the subclass drink price
+     * roundToTwo  rounds a Double to two decimal places
+     * drinkRecipe a map, Key ingredient, Value amount of ingredient
+     */
     String drinkName; 
     Double drinkPrice;
     DecimalFormat roundToTwo = new DecimalFormat(".00");
     Map<Ingredient, Integer> drinkRecipe = new HashMap<Ingredient, Integer>();
     
-    public String getDrinkName() {
-        return drinkName;
-    }
-    
+    /**
+     * The Drink class constructor.
+     * 
+     * @param drinkName a string passed from the subclass 
+     */
     protected Drink(String drinkName){
         this.drinkName = drinkName;
     }
     
+    /**
+     * Retrieves the drink name
+     *
+     * @return      the drink name
+     */
+    public String getDrinkName() {
+        return drinkName;
+    }
+    
+    /**
+     * Retrieves the recipe of the drink.
+     * 
+     * @return      returns a map, Key ingredient, Value amount of ingredient
+     */
     public Map<Ingredient, Integer> getDrinkRecipe() {
         return drinkRecipe;
     }
     
+    /**
+     * Retrieves the price of the drink
+     * 
+     * @return      a string version of the price
+     */
     public String getDrinkPrice(){
         this.drinkPrice = 0.0;
         for(Ingredient entry : drinkRecipe.keySet()) {
@@ -40,6 +68,9 @@ public abstract class Drink implements Comparable<Drink>{
         return roundToTwo.format(drinkPrice);
     }
     
+    /**
+     * Overridden method to sort the Drink class
+     */
     @Override 
     public int compareTo(Drink drink) {
         return drinkName.compareTo(drink.getDrinkName());
